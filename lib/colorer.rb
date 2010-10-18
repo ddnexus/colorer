95c4cf0e8e6fe97b1cbe3f9752620aed20dc1aac
Domizio Demichelis <dd.nexus@gmail.com>
2010-10-17 21:35:59 -0400

    added mixed basic and custom styles feature

--- lib/colorer.rb
+++ lib/colorer.rb
@@ -2,7 +2,17 @@ module Colorer
 
   VERSION = File.read(File.expand_path('../../VERSION', __FILE__)).strip
 
-  class Exception < ::Exception; end
+  class AlreadyDefinedMethod < Exception
+    def initialize(meth, klass)
+      super("already defined method '#{meth}' for #{klass}:#{klass.class}")
+    end
+  end
+
+  class UnknownSgrCode < Exception
+    def initialize(sgr)
+      super("#{sgr.inspect} is unknown")
+    end
+  end
 
   # Select Graphic Rendition
   BASIC_SGR = {
@@ -31,35 +41,33 @@ module Colorer
     :onwhite   => 47
   }
 
+  extend self
+
   @color = true
-  class << self
-    attr_accessor :color
+  attr_accessor :color
 
-    def add_sgr(string, sgr)
-      return string unless (Colorer.color && STDOUT.tty? && ENV['TERM'] && ENV['TERM'] != 'dumb')
-      string = sprintf("\e[0m%s\e[0m", string) unless string =~ /^\e\[[\d;]+m.*\e\[0m$/
-      sgr = Colorer::BASIC_SGR[sgr] if sgr.is_a?(Symbol)
-      raise Exception, "undefined SGR code '#{sgr}'" if sgr.nil?
-      string.sub /^(\e\[[\d;]+)/, '\1;' + sgr.to_s
-    end
+  def add_sgr(string, sgr)
+    return string unless (Colorer.color && STDOUT.tty? && ENV['TERM'] && ENV['TERM'] != 'dumb')
+    code = sgr.is_a?(Symbol) ? Colorer::BASIC_SGR[sgr] : sgr
+    raise UnknownSgrCode.new(sgr) unless code.is_a? Integer
+    string = sprintf("\e[0m%s\e[0m", string) unless string =~ /^\e\[[\d;]+m.*\e\[0m$/
+    string.sub /^(\e\[[\d;]+)/, '\1;' + code.to_s
+  end
 
-    def define_styles(styles, force=false)
+  def define_styles(styles, force=false)
+    styles.each_pair do |meth, style|
       String.class_eval do
-        if styles.delete(:basic)
-          Colorer::BASIC_SGR.each_pair do |meth, sgr|
-            raise Exception, "already defined method '#{meth}' for #{self}:#{self.class}" \
-              if !force && instance_methods.include?(meth.to_s)
-            define_method(meth) do
+        if meth == :basic && style
+          Colorer::BASIC_SGR.each_pair do |m, sgr|
+            raise AlreadyDefinedMethod.new(m, self) if !force && method_defined?(m)
+            define_method(m) do
               Colorer.add_sgr(self, sgr)
             end
           end
         else
-          styles.each_pair do |meth, style|
-            raise Exception, "already defined method '#{meth}' for #{self}:#{self.class}" \
-              if !force && instance_methods.include?(meth.to_s)
-            define_method(meth) do
-              style.inject(self) { |str, sgr|  Colorer.add_sgr(str, sgr) }
-            end
+          raise AlreadyDefinedMethod.new(meth, self) if !force && method_defined?(meth)
+          define_method(meth) do
+            style.inject(self) { |str, sgr| Colorer.add_sgr(str, sgr) }
           end
         end
       end
